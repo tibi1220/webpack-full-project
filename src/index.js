18dbdea2b77d81282f68fe5ec979baff01827f53
Tibor Sandor <tiborsandor2001@gmail.com>
2021-07-16 22:29:48 +0200

    add css, hotreloading

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "./styles/index.scss";
+
 const elvenShieldRevipe = {
   leatherStrips: 2,
   ironIgnot: 1,
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
   mode: "development",
+  target: "web",
 
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.s?css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.js$/i,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
@@ -13,8 +25,11 @@ module.exports = {
     ],
   },
 
+  plugins: [new MiniCssExtractPlugin()],
+
   devtool: "source-map",
   devServer: {
     contentBase: "./dist",
+    hot: true,
   },
 };
