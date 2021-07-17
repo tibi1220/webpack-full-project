26d1732d68fe1edc8955d8237da4c017ac6cba94
Tibor Sandor <tiborsandor2001@gmail.com>
2021-07-17 10:47:17 +0200

    add image support

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,38 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
 
 module.exports = {
   mode: 'development',
   target: 'web',
 
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'images/[hash][ext][query]',
+  },
+
   module: {
     rules: [
       {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+        /* parser: {
+          dataUrlCondition: {
+            maxSize: 30 * 1024,
+          },
+        }, */
+        /**
+         * asset/resource keeps files
+         * asset/inline puts it into JS
+         * asset decides it for you
+         */
+      },
+      {
         test: /\.s?css$/i,
         use: [
-          MiniCssExtractPlugin.loader,
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: '' },
+          },
           'css-loader',
           'postcss-loader',
           'sass-loader',
